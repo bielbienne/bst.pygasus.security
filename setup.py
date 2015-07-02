2126d315a89df21bebac830eb2669c2254b7db48
Samuel Riolo <samuel.riolo@biel-bienne.ch>
2015-07-02 14:19:00 +0200

    cleanup egg structure

--- setup.py
+++ setup.py
@@ -4,30 +4,29 @@ import os
 version = '1.0'
 
 long_description = (
-    open('README.txt').read()
+    open('README.rst').read()
     + '\n' +
-    'Contributors\n'
-    '============\n'
-    + '\n' +
-    open('CONTRIBUTORS.txt').read()
-    + '\n' +
-    open('CHANGES.txt').read()
+    open('CHANGES.rst').read()
     + '\n')
 
 setup(name='bst.pygasus.security',
       version=version,
-      description="Python Framework for ExtJs",
+      description="provide authentication for bst.pygasus framework",
       long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-          "Programming Language :: Python",
+          'Programming Language :: Python :: 3',
+          'Natural Language :: English',
+          'License :: OSI Approved :: Zope Public License',
+          'Operating System :: OS Independent',
+          'Development Status :: 4 - Beta'
       ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='gpl',
+      keywords='pygasus web framework extjs',
+      author='codeix',
+      author_email='samuel.riolo@biel-bienne.ch',
+      url='https://github.com/bielbienne/bst.pygasus.security',
+      license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       namespace_packages=['bst', 'bst.pygasus'],
